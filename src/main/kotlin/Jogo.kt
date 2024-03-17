e6232571a5dd2c243bcd6cee893cbdd4181b84fe
PatrezeLeal <patrezeleal@gmail.com>
2024-03-17 17:39:01 -0300

    coloquei data antes de casa classe "class" em InfoJogo.kt e jogo.kt tirei os @serialaze de jogo.kt,e no main foi criada a variável meuInfoJogo para receber as informações tratadas pela biblioteca gson dos dados em formato json, na classe infoJogo recebo todas as informações da api sheepSharks sobre o jogo do batman, em infoApiSharks eu pego so as informções que eu quero que e o título e a capa e em JOgo.kt eu deixo as informações que vão aparecer no console da forma que eu quero.

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,14 +1,10 @@
-import com.google.gson.annotations.SerializedName
-
-class Jogo (@SerializedName("title")   val titulo:String,
-            @SerializedName("thumb") var capa:String ){
+data class Jogo (val titulo : String, val capa : String) {
     val descricao = ""
     override fun toString(): String {
         return "Meu jogo:\n" +
                 "Título: $titulo\n" +
                 "Capa: $capa\n" +
-                "Descricao: $descricao"
+                "Descrição: $descricao"
     }
-
 
 }
