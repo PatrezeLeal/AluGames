a3be49861bc68829979cb512cd2fc5d877d54c6f
PatrezeLeal <patrezeleal@gmail.com>
2024-03-24 16:29:34 -0300

    mudei coisas.

--- src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -10,7 +10,11 @@ fun main(){
     gamer1.let {
         it.dataNascimento = "18/09/2000"
         it.usuario="jaqueskywalker"
-        it.idInterno="jaqueskywalker1234"
+
+    }.also{
+        println(gamer1.idInterno)
     }
+    println(gamer1)
+    gamer1.usuario="jacque"
     println(gamer1)
 }
