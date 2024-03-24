39921b5149f4b7a4dc2dcf4bbdb043afc29ae5e6
PatrezeLeal <patrezeleal@gmail.com>
2024-03-24 17:21:57 -0300

    mudei coisas.

--- src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -1,7 +1,7 @@
 import br.com.alura.alugames.modelo.Gamer
 
 fun main(){
-    val gamer1 = Gamer( "Jaque", "Jacque@email.com")
+    val gamer1 = Gamer( "  ", "Jacque@email.com")
     println(gamer1)
 
     val gamer2 = Gamer("Jaque", "Jacque@email.com","19/19/1992", "jeniblo")
